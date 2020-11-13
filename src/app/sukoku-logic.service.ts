8b2b8456d1c5aeabbce21d6b2907778fcd98cfe8
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-13 16:28:17 -0600

    successfully generated randomized sudoku values

--- src/app/sukoku-logic.service.ts
+++ src/app/sukoku-logic.service.ts
@@ -16,9 +16,7 @@ export class SukokuLogicService {
   buildBoard(): Array<Array<BoardPiece>> {
     let board: Array<Array<BoardPiece>> = [];
     board = this.buildRows(board);
-    board = this.assignValues(board);
-    this.generateValues();
-    // console.log(board);
+    board = this.generateValues(board);
     return board;
   }
 
@@ -27,18 +25,6 @@ export class SukokuLogicService {
       board.push([]);
       for (let x = 0; x < 9; x ++) {
         board[i].push({value: 0, visible: false});
-      }
-    }
-    return board;
-  }
-
-  assignValues(board: Array<Array<BoardPiece>>): Array<Array<BoardPiece>> {
-    for (let i = 0; i < 9; i++) {
-      let values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      for (let x = 0; x < 9; x++) {
-        let random = this.randomValue(values.length);
-        board[i][x].value = values[random];
-        values.splice(random, 1);
       }
     }
     return board;
@@ -58,24 +44,26 @@ export class SukokuLogicService {
     if (i > 6) {
       modifier++;
     }
+    if (modifier > 9) {
+      modifier - 9;
+    }
     return modifier;
   }
 
-  generateValues(): Array<Array<number>> {
-    let values = [];
+  generateValues(values: Array<Array<BoardPiece>>): Array<Array<BoardPiece>> {
+    let rand = this.randomValue(10);
     for (let i = 1; i < 10; i++) {
-      values.push([]);
-      for (let x = 1; x < 10; x++) {
+      for (let x = rand; x < rand+9; x++) {
        
         let value = (x+this.generateModifier(i));
 
         if (value > 9) {
           value = value - 9;
         }
-
-        values[i-1].push(value);
+        values[i-1][x-rand].value = value;
       }
     }
+    console.log(values);
     return values;
   }
 
