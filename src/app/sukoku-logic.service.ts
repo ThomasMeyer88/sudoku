dc82b316e685cb1f778d2b2c0922b71e7c310863
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-15 15:19:26 -0600

    delete failed matrix

--- src/app/sukoku-logic.service.ts
+++ src/app/sukoku-logic.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CheckboxControlValueAccessor } from '@angular/forms';
-import { throwError } from 'rxjs';
 import { BoardPiece } from './models/boardPiece';
 
 @Injectable({
@@ -22,15 +20,12 @@ export class SukokuLogicService {
   }
 
   fillBoard(board: Array<Array<BoardPiece>>): Array<Array<BoardPiece>> {
-    for (let i = 0; i < 3; i++) {
-      for (let x = 0; x < 3; x++) {
+    for (let i = 0; i < 2; i++) {
+      for (let x = 0; x < 2; x++) {
         // 012 345 678
         let startRow = i*3;
         let startCol = x*3;
         board = this.generateMatrix(board, startRow, startCol);
-        if (!board) {
-          break;
-        }
       }
     }
     return board;
@@ -59,26 +54,34 @@ export class SukokuLogicService {
 
     let matrixValues = [];
  
-    for (let i = startRow; i < startRow+3; i++) {
-      for (let x = startCol; x < startCol+3; x++) {
+    for (let i = startRow; i < startRow + 3; i++) {
+      for (let x = startCol; x < startCol + 3; x++) {
 
         let rowVals = this.getRowVals(board, i);
 
         let colVals = this.getColVals(board, x);
 
-        // console.log(i, x);
         let potentialValues = this.removedUsedValues([rowVals, colVals, matrixValues]);
 
         if (potentialValues.length == 0) {
           console.log(`%c No Value Assignable`, 'color: red; font-weight: bold');
           console.log(`%c Failed at ${startRow}, ${startCol}`, 'color: red; font-weight: bold');
-          // break;
+          this.deleteFromBoard(board, startRow, startCol);
+          break;
         }
 
         let rand = this.randomValue(potentialValues.length);
         board[i].push(this.newBoardPiece(potentialValues[rand], false));
         matrixValues.push(potentialValues[rand]);
       }
+    }
+    return board;
+  }
+
+  deleteFromBoard(board: Array<Array<BoardPiece>>, startRow: number, 
+    startCol: number): Array<Array<BoardPiece>> {
+    for (let i = startRow; i < startRow + 3; i++) {
+        board[i].splice(startCol, board[i].length);
     }
     return board;
   }
