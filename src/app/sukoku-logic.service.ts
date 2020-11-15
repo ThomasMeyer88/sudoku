7963f20b93a5f0ccabddc787fa48b7932250a553
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-15 13:10:28 -0600

    Update sukoku-logic.service.ts

--- src/app/sukoku-logic.service.ts
+++ src/app/sukoku-logic.service.ts
@@ -20,32 +20,41 @@ export class SukokuLogicService {
   }
 
   fillBoard(board: Array<Array<BoardPiece>>): Array<Array<BoardPiece>> {
-    for (let i = 0; i < 1; i++) {
-      let rowVals = [];
-      for (let x = 0; x < 1; x++) {
-        let colVals = [];
+    for (let i = 0; i < 3; i++) {
+
+      for (let x = 0; x < 3; x++) {
         // 012 345 678
         let startRow = i*3;
         let startCol = x*3;
 
-        board = this.generateMatrix(board, colVals, rowVals, startRow, startCol);
+        board = this.generateMatrix(board, startRow, startCol);
       }
     }
     return board;
   }
 
-  generateMatrix(board: Array<Array<BoardPiece>>, colVals: Array<number>, 
-    rowVals: Array<number>, startRow: number, startCol: number): Array<Array<BoardPiece>> {
-    let potentialValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let adjRowVals = this.removedUsedValues(rowVals, potentialValues);
-    let adjColVals = this.removedUsedValues(colVals, potentialValues);
-    let matrixValues = this.removedUsedValues([], potentialValues);
+  updateRowVals(board: Array<Array<BoardPiece>>, rowVals: Array<number>, 
+    startRow: number): Array<number> {
+    board[startRow].forEach((x) => {
+      rowVals.push(x.value);
+    })
+    return rowVals;
+  }
 
+  generateMatrix(board: Array<Array<BoardPiece>>, startRow: number, 
+                startCol: number): Array<Array<BoardPiece>> {
+    let potentialValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let matrixValues = this.removedUsedValues([], potentialValues);
     for (let i = startRow; i < startRow+3; i++) {
+      let rowVals = this.updateRowVals(board, [], i);
+      let usuableValues = this.removedUsedValues(rowVals, matrixValues);
+
       for (let x = startCol; x < startCol+3; x++) {
-        let rand = this.randomValue(matrixValues.length);
-        board[i].push(this.newBoardPiece(matrixValues[rand], false));
-        matrixValues.splice(rand, 1);
+        console.log(`%c Row: ${i}, Col ${x}`, 'color: blue');
+        console.log(usuableValues);
+        let rand = this.randomValue(usuableValues.length);
+        board[i].push(this.newBoardPiece(usuableValues[rand], false));
+        usuableValues.splice(rand, 1);
       }
     }
     return board;
