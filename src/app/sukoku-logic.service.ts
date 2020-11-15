31be35305985e8fbf33847b3abe63895847a1f9b
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-14 19:35:58 -0600

    function for removing values already assigned to xyz

--- src/app/sukoku-logic.service.ts
+++ src/app/sukoku-logic.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CheckboxControlValueAccessor } from '@angular/forms';
 import { BoardPiece } from './models/boardPiece';
 
 @Injectable({
@@ -22,25 +23,39 @@ export class SukokuLogicService {
     for (let i = 0; i < 3; i++) {
       for (let x = 0; x < 3; x++) {
         // 012 345 678
-        board = this.generateMatrix(board, [], [], [], i*3, x*3);
+        board = this.generateMatrix(board, [1, 2], [4, 5], [7], i*3, x*3);
       }
     }
     return board;
   }
 
-  generateMatrix(board: Array<Array<BoardPiece>>, matUsed: Array<number>, colUsed: Array<number>, 
-    rowUsed: Array<number>, startRow: number, startCol: number): Array<Array<BoardPiece>> {
-    let potentialValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  generateMatrix(board: Array<Array<BoardPiece>>, matVals: Array<number>, colVals: Array<number>, 
+    rowVals: Array<number>, startRow: number, startCol: number): Array<Array<BoardPiece>> {
+
+    let potentialValues = this.removedUsedValues(matVals, colVals, rowVals);
     for (let i = startRow; i < startRow+3; i++) {
       for (let x = startCol; x < startCol+3; x++) {
-        console.log(i, x);
         let rand = this.randomValue(potentialValues.length);
         board[i].push(this.newBoardPiece(potentialValues[rand], false));
         potentialValues.splice(rand, 1);   
-        console.log(board);   
       }
     }
     return board;
+  }
+
+  removedUsedValues(matVals: Array<number>, colVals: Array<number>, rowVals: Array<number>) {
+    let potentialValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    matVals.forEach((x) => {
+      potentialValues.splice(potentialValues.indexOf(x), 1);
+    });
+    colVals.forEach((x) => {
+      potentialValues.splice(potentialValues.indexOf(x), 1);
+    });
+    rowVals.forEach((x) => {
+      potentialValues.splice(potentialValues.indexOf(x), 1);
+    });
+    console.log(potentialValues);
+    return potentialValues;
   }
 
   newBoardPiece(value: number, visible: Boolean): BoardPiece {
