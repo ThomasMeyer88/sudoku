f8fa9a1a4b8e6d0356189557bf42d6ee0fe110a9
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-14 19:28:34 -0600

    successfully building board through matrices

--- src/app/sukoku-logic.service.ts
+++ src/app/sukoku-logic.service.ts
@@ -5,7 +5,6 @@ import { BoardPiece } from './models/boardPiece';
   providedIn: 'root'
 })
 export class SukokuLogicService {
-  matrix = [[], [], []];
   constructor() { }
 
   generateBoard() {
@@ -14,27 +13,34 @@ export class SukokuLogicService {
   }
 
   buildBoard(): Array<Array<BoardPiece>> {
-    let board: Array<Array<BoardPiece>> = [];
+    let board: Array<Array<BoardPiece>> = [[], [], [], [], [], [], [], [], []];
     board = this.fillBoard(board);
     return board;
   }
 
   fillBoard(board: Array<Array<BoardPiece>>): Array<Array<BoardPiece>> {
-    board = this.generateFirstMatrix();
+    for (let i = 0; i < 3; i++) {
+      for (let x = 0; x < 3; x++) {
+        // 012 345 678
+        board = this.generateMatrix(board, [], [], [], i*3, x*3);
+      }
+    }
     return board;
   }
 
-  generateFirstMatrix(): Array<Array<BoardPiece>> {
-    let matrix = this.matrix;
+  generateMatrix(board: Array<Array<BoardPiece>>, matUsed: Array<number>, colUsed: Array<number>, 
+    rowUsed: Array<number>, startRow: number, startCol: number): Array<Array<BoardPiece>> {
     let potentialValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let i = 0; i < 3; i++) {
-      for (let x = 0; x < 3; x++) {
+    for (let i = startRow; i < startRow+3; i++) {
+      for (let x = startCol; x < startCol+3; x++) {
+        console.log(i, x);
         let rand = this.randomValue(potentialValues.length);
-        matrix[i].push(this.newBoardPiece(potentialValues[rand], false));
-        potentialValues.splice(rand, 1);      
+        board[i].push(this.newBoardPiece(potentialValues[rand], false));
+        potentialValues.splice(rand, 1);   
+        console.log(board);   
       }
     }
-    return matrix;
+    return board;
   }
 
   newBoardPiece(value: number, visible: Boolean): BoardPiece {
