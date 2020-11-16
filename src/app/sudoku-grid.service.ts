409059bd05d80f9790c6ef60d1a74f91b37d5cda
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-16 11:49:02 -0600

    function to validate col

--- src/app/sudoku-grid.service.ts
+++ src/app/sudoku-grid.service.ts
@@ -22,10 +22,22 @@ export class SudokuGridService {
     return sudokuGrid;
   }
 
-  getRowSum(row: number, grid: Array<Array<BoardPiece>>): Boolean {
+  checkRow(row: number, grid: Array<Array<BoardPiece>>): Boolean {
     let sum: number = 0;
     for (let i = 0; i < 9; i++) {
       sum += grid[row][i].value;
+    }
+    if (sum == 55) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkCol(col: number, grid: Array<Array<BoardPiece>>): Boolean {
+    let sum: number = 0;
+    for (let i = 0; i < 9; i++) {
+      sum += grid[i][col].value;
     }
     if (sum == 55) {
       return true;
