f71e55d1994bb934fe5cc183617f211a016c5695
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-11-19 14:38:46 -0600

    generate board with 17-22 visible values

--- src/app/sukoku-logic.service.ts
+++ src/app/sukoku-logic.service.ts
@@ -12,11 +12,35 @@ export class SukokuLogicService {
 
 
   buildBoard(): Array<Array<BoardPiece>> {
-    let board: Array<Array<BoardPiece>> = [[], [], [], [], [], [], [], [], []];
+    let board: Array<Array<BoardPiece>> = this.getEmptyBoard();
+    board = this.addStartValues(board);
+    return board;
+  }
+
+  addStartValues(board: Array<Array<BoardPiece>>): Array<Array<BoardPiece>> {
+    let visible = 0;
+    for (let i = 0; i < 9; i++) {
+      for (let x = 0; x < 9; x++ ) {
+        if (this.randomValue(4) == 1) {
+          visible++;
+          board[i].push(this.newBoardPiece(x+1, true));
+        } else {
+          board[i].push(this.newBoardPiece(0, false));
+        }
+      }
+    }
+    console.log(visible);
+    if (visible < 17 || visible > 22) {
+      board = this.getEmptyBoard();
+      this.addStartValues(board);
+    }
     return board;
   }
 
 
+  getEmptyBoard(): Array<Array<BoardPiece>> {
+    return [[], [], [], [], [], [], [], [], []];
+  }
 
   newBoardPiece(value: number, visible: Boolean): BoardPiece {
     return {value: value, visible: visible};
